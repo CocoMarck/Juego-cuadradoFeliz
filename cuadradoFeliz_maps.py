ccedd8062be968754a80fabfb77150493137c685
CocoMarck <76503881+CocoMarck@users.noreply.github.com>
2024-09-26 18:57:54 -0700

    Add files via upload

--- cuadradoFeliz_maps.py
+++ cuadradoFeliz_maps.py
@@ -13,6 +13,18 @@ clock = pygame.time.Clock()
 
 
 
+# Surface | Para mostrar los objetos en solo en una perte de la pantalla
+# Se utilizara size_display_edit, para el funcionamiento del scroll
+# Se usara difference_display_edit, para el posicionamiento del displa edit en el display. Y tambien para la pos del mouse al aplicar un evento.
+size_display_edit = [ int(data_CF.disp[0]*0.8), int(data_CF.disp[1]*0.8) ]
+difference_display_edit = [ 
+    (data_CF.disp[0]-size_display_edit[0])//2,
+    (data_CF.disp[1]-size_display_edit[1])//2
+]
+display_edit = pygame.Surface( size_display_edit )
+
+
+
 
 # Objetos
 layer_all_sprites = pygame.sprite.LayeredUpdates()
@@ -66,11 +78,32 @@ class object_grid( pygame.sprite.Sprite ):
         self.rect = self.surf.get_rect( topleft=position )
         layer_all_sprites.add(self, layer=0)
         grid_objects.add(self)
+        
+        # Tipo de objeto
+        self.type_object = (
+            current_map.list_map
+            [ (self.rect.y) //data_CF.pixel_space ]
+            [ (self.rect.x) //data_CF.pixel_space ]
+        )
+        self.xy_spawn = [ (self.rect.y) //data_CF.pixel_space,  (self.rect.x) //data_CF.pixel_space ]
+        
+    def update_type_object(self):
+        if self.type_object == '.':
+            self.surf = pygame.Surface( (self.rect.width, self.rect.height), pygame.SRCALPHA )
+            self.surf.fill( (0, 0, 0, 0) )
+            
+        # Establecer nuevo tipo de texto en el texto a guardar/cambiar
+        current_map.list_map [self.xy_spawn[0]] [self.xy_spawn[1]] = self.type_object
 
 
 
 
 # Generador de cuadritos para hacer el mapa
+dict_object = {
+    'space': '.',
+    'plataform': 'p'
+}
+
 current_map = Map
 read_Map( current_map, level=data_CF.current_level )
 #read_Map( current_map, level='./resources/maps/cf_map_default.txt' )
@@ -99,7 +132,7 @@ for line in current_map.list_map:
             '#': [None, None],
             '|': ['limit', None],
 
-            'j': ['player_not-move', 2],
+            'j': ['icon', None],
 
             'p': ['stone', None],
             'P': ['stone', 2 ],
@@ -141,27 +174,35 @@ for line in current_map.list_map:
                         object_grid(
                             position=position, size=size, image=dict_preset[preset][0] 
                         )
+
+                        # Establecer coordenadas xy spawn de jugador
+                        if dict_preset[preset][0] == 'icon':
+                            if player_spawn_xy[0] == 0:
+                                player_spawn_xy[0] = position[0]
+                            if player_spawn_xy[1] == 0:
+                                player_spawn_xy[1] = position[1]
                     else:
                         # Establecer nuevo tamaño
                         size = [
                             data_CF.pixel_space*dict_preset[preset][1], 
                             data_CF.pixel_space*dict_preset[preset][1]
-                        ]
-                        
-                        # Detectar si es el sprite del jugador o no
-                        if dict_preset[preset][0] == 'player_not-move':
-                            new_pos = [
-                                position[0]-size[0]/4,
-                                position[1]-size[1]/2
-                            ]
-                            player_spawn_xy = new_pos
-                        else:
-                            new_pos = position
+                        ]   
                             
                         # Establecer objeto
                         object_grid(
-                            position=new_pos, size=size, image=dict_preset[preset][0]
+                            position=position, size=size, image=dict_preset[preset][0]
                         )
+
+
+
+
+def return_map( Map ):
+    save_this_text = ''
+    for line in Map.list_map:
+        for char in line:
+            save_this_text += char
+        save_this_text += '\n'
+    return save_this_text[:-1]
 
 
 
@@ -174,6 +215,8 @@ scroll_float = [0,0]
 
 # Posición de camara
 camera_xy = player_spawn_xy
+click_left = False
+click_right = False
 move_up = False
 move_down = False
 move_left = False
@@ -185,8 +228,8 @@ move_right = False
 # Función Scroll/Camara | Posicionar camara en donde esta el jugador
 def start_camera():
     scroll_float = [0,0]
-    scroll_float[0] += (camera_xy[0] -scroll_float[0] -data_CF.disp[0]/2)
-    scroll_float[1] += (camera_xy[1] -scroll_float[1] -data_CF.disp[1]/2)
+    scroll_float[0] += (camera_xy[0] -scroll_float[0] -size_display_edit[0]/2)
+    scroll_float[1] += (camera_xy[1] -scroll_float[1] -size_display_edit[1]/2)
     
     return [int(scroll_float[0]), int(scroll_float[1])]
                         
@@ -208,14 +251,8 @@ while exec_game:
             if left or right:
                 # Click izquierdo
                 mouse_pos = pygame.mouse.get_pos()
-                for obj in grid_objects:
-                    if obj.rect.collidepoint( mouse_pos[0]+scroll_int[0], mouse_pos[1]+scroll_int[1] ):
-                        # Click en grid
-                        print( obj.rect.x, obj.rect.y ) # Posicion en pixeles
-                        print( 
-                            (obj.rect.x) //data_CF.pixel_space, 
-                            (obj.rect.y) //data_CF.pixel_space 
-                        ) # Posición en cuadros/rejillas/cuadriculas/mallas/
+                click_left = True
+                click_right = False
             if middle:
                 # Click Rueda de mouse
                 mouse_movement = pygame.mouse.get_rel()
@@ -230,6 +267,8 @@ while exec_game:
                 if mouse_movement[0] > 0:
                     move_right = True
         if event.type == pygame.MOUSEBUTTONUP:
+            click_right = False
+            click_left = False
             move_up = False
             move_down = False
             move_left = False
@@ -261,7 +300,8 @@ while exec_game:
     
     
     # Limpiar pantalla (agregar esta línea)
-    display.fill( generic_colors('green') )
+    display.fill( generic_colors('black') )
+    display_edit.fill( generic_colors('green') )
     
     
     
@@ -281,42 +321,71 @@ while exec_game:
     
     
     # Función scroll | Camara
-    scroll_float[0] += (camera_xy[0] -scroll_float[0] -data_CF.disp[0]/2)/4
-    scroll_float[1] += (camera_xy[1] -scroll_float[1] -data_CF.disp[1]/2)/4
+    scroll_float[0] += (camera_xy[0] -scroll_float[0] -size_display_edit[0]/2)/4
+    scroll_float[1] += (camera_xy[1] -scroll_float[1] -size_display_edit[1]/2)/4
     scroll_int = [int(scroll_float[0]), int(scroll_float[1])]
     
+    
+    
+    # Función | Mouse | Cuando se hace click a un objeto
+    if click_left == True or click_right == True:
+        for obj in grid_objects:
+            # Calcular la posición relativa del objeto dentro de la vista previa
+            rel_pos_x = obj.rect.x - scroll_int[0]
+            rel_pos_y = obj.rect.y - scroll_int[1]
+
+            # Comprobar si el objeto está dentro de la vista previa (display_edit)
+            if (0 <= rel_pos_x < size_display_edit[0]) and (0 <= rel_pos_y < size_display_edit[1]):
+                if obj.rect.collidepoint(
+                    mouse_pos[0] - difference_display_edit[0] + scroll_int[0],
+                    mouse_pos[1] - difference_display_edit[1] + scroll_int[1]
+                ):
+                    # Si el objeto está en la vista y se hace clic, realizar la acción
+                    print(obj.rect.x, obj.rect.y)  # Posición en píxeles
+                    print(obj.type_object)
+                    obj.type_object = '.'
+                    obj.update_type_object()
+    '''
+    if click_left == True or click_right == True:
+        for obj in grid_objects:
+            if obj.rect.collidepoint( 
+                mouse_pos[0]-difference_display_edit[0] +scroll_int[0],
+                mouse_pos[1]-difference_display_edit[1] +scroll_int[1]
+            ):
+                # Click en grid
+                # Obtener menu de objetos y seleccionar uno
+                print( obj.rect.x, obj.rect.y ) # Posicion en pixeles
+                print( obj.type_object )
+                obj.type_object = '.'
+                obj.update_type_object()
+    '''
+
     
     
     
     # Objetos / Mostrar / Todos los sprites
     for sprite in layer_all_sprites.sprites():
         # Detectar que el sprite no sebrepase la pantalla
-        sprite_goes_off_screen = False
-        if (
-            (sprite.rect.x-scroll_int[0] > data_CF.disp[0]) or 
-            (sprite.rect.x-scroll_int[0] < 0)
-        ):
-            sprite_goes_off_screen = True
-        if (
-            (sprite.rect.y-scroll_int[1] > data_CF.disp[1]) or 
-            (sprite.rect.y-scroll_int[1] < 0)
-        ):
-            sprite_goes_off_screen = True
+        display_collision = scroll_display_collision(
+            [sprite.rect.x, sprite.rect.y], scroll_int, size_display_edit, [data_CF.pixel_space, 0]
+        )
 
         # Si el esprite esta en pantalla, mostrarlo.
-        if sprite_goes_off_screen == False:
-            display.blit(
-                sprite.surf, 
-                (
-                    sprite.rect.x -scroll_int[0],
-                    sprite.rect.y -scroll_int[1]
+        if display_collision == None:
+            display_edit.blit(
+                sprite.surf, (
+                    sprite.rect.x -scroll_int[0], sprite.rect.y -scroll_int[1]
                 )
             )
     
     
     
+    # Mostrar display edit
+    display.blit( display_edit, (difference_display_edit[0], difference_display_edit[1]) )
     
     # Fin
     pygame.display.update()
     clock.tick( data_CF.fps )
 pygame.quit()
+
+print( return_map( current_map ) )
